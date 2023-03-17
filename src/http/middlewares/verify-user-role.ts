78019c1fd4dbe29dc35a5469cbe632b012e18fc8
marcelino <marcelinoneto34@gmail.com>
2023-03-17 09:59:59 -0300

    RBAC

---
+++ src/http/middlewares/verify-user-role.ts
@@ -0,0 +1,13 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export function verifyUserRole(roleToVerify: 'ADMIN' | 'MEMBER') {
+  return async (request: FastifyRequest, replay: FastifyReply) => {
+    const { role } = request.user
+
+    if (role !== roleToVerify) {
+      return replay.status(401).send({
+        message: 'Unauthorize.',
+      })
+    }
+  }
+}
