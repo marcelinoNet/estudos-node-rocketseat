568ae2d5044463962209227bc3a27d56b686c0ea
marcelino <marcelinoneto34@gmail.com>
2023-03-17 09:35:59 -0300

    Refresh Token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -31,9 +31,27 @@ export async function authenticate(
       },
     )
 
-    return replay.status(200).send({
-      token,
-    })
+    const refreshToken = await replay.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return replay
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return replay.status(400).send({
